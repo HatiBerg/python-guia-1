0a4c0394c7688fc2f9a5a5b499ab846204744949
Esteban Adolfo Ponce Lira <66049549+HatiBerg@users.noreply.github.com>
2023-08-19 18:01:01 -0400

    Update ejercicio_1.py

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,3 +1,16 @@
 # Cree un programa que elija un numero al azar entre 1 y 100, luego le pregunta por un número y el programa le debe decir si 
 # el numero ingresado es demasiado bajo o demasiado alto, hasta que logre dar con el numero generado.
 
+import random
+
+numero_random = random.randint(1,100)
+
+while(True):
+    numero = int(input("Ingrese un número: "))
+    if numero > numero_random:
+        print("El numero ingresado es demasiado alto")
+    elif numero < numero_random:
+        print("El numero ingresado es demasiado bajo")
+    else:
+        print("Encontraste el numero generado aleatoriamente")
+        break
