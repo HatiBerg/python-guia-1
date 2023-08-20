639e49f6852b4979c2c19eb6941ded918aeaab47
Esteban Adolfo Ponce Lira <66049549+HatiBerg@users.noreply.github.com>
2023-08-20 06:04:19 -0400

    Update ejercicio_10.py

--- ejercicio_10.py
+++ ejercicio_10.py
@@ -5,8 +5,27 @@
 
 def main():
     lista_compra = {}
-
+    total = 0
+    print("LISTA DE COMPRA")
+    print("ESCRIBA TERMINAR PARA FINALIZAR LA LISTA DE COMPRA")
+    articulo = input("Ingrese el nombre del artículo: ")
     
+    while(articulo.lower() != "terminar"):
+        precio = int(input("Ingrese el precio del artículo: "))
+        lista_compra[articulo] = precio
+        print("")
+        articulo = input("Ingrese el nombre del artículo: ")
+    print("")
+
+    if len(lista_compra) == 0:
+        print("La lista de compra esta vacía")
+    else:
+        print("Lista de compra")
+        print("")
+        for i in lista_compra:
+            print(f"{i}   {lista_compra[i]}")
+            total+=lista_compra[i]
+        print(f"Total   {total}")
 
 if __name__ == '__main__':
     main()
